eda97f9b145ad254a466144d0c69437aa6e6ab54
wuboluo <383049891@qq.com>
2022-10-20 14:08:39 +0800

    Item list 部分布局

--- Assets/Editor/UI Builder/ItemEditor.cs
+++ Assets/Editor/UI Builder/ItemEditor.cs
@@ -6,7 +6,7 @@ using UnityEditor.UIElements;
 
 public class ItemEditor : EditorWindow
 {
-    [MenuItem("Window/UI Toolkit/ItemEditor")]
+    [MenuItem("Yang/Item Editor")]
     public static void ShowExample()
     {
         ItemEditor wnd = GetWindow<ItemEditor>();
@@ -18,20 +18,11 @@ public class ItemEditor : EditorWindow
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
 
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
-
         // Import UXML
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UI Builder/ItemEditor.uxml");
         VisualElement labelFromUXML = visualTree.Instantiate();
         root.Add(labelFromUXML);
 
-        // A stylesheet can be added to a VisualElement.
-        // The style will be applied to the VisualElement and all of its children.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/UI Builder/ItemEditor.uss");
-        VisualElement labelWithStyle = new Label("Hello World! With Style");
-        labelWithStyle.styleSheets.Add(styleSheet);
-        root.Add(labelWithStyle);
+        
     }
 }
